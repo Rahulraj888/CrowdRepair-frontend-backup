5faa04570d5b10e1c5c67f0142e5feb019f7eb5c
BaluRanaMagar <hactorranamagar@gmail.com>
2025-06-06 17:41:22 -0400

    first code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Mobile Appz. All rights reserved.</p>
+      <p>© 2026 Mobile Appz. All rights reserved.</p>
     </footer>
   );
 }
