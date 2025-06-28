d6a4817e63a87aa927ae97bd3a4d41882d432e9d
Rahulraj888 <reddavenirahul@gmail.com>
2025-06-27 21:43:17 -0400

    admin page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <header className="shadow-sm mb-0 pb-0">
-      <nav className="navbar navbar-expand-lg navbar-light bg-white px-3 py-2">
+      <nav className="navbar navbar-expand-lg navbar-light px-3 py-2">
         <div className="container-fluid">
           {/* Logo */}
           <Link className="navbar-brand d-flex align-items-center" to="/">
